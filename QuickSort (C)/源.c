7f0a041fafb1a2526c4052a00b0ab7dd2a6410c0
hhzzjj <37446568+hhzzjj@users.noreply.github.com>
2019-02-13 15:58:48 +0800

    Add files via upload

---
+++ QuickSort (C)/源.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "quicksort.h"
+
+
+void main()
+{
+	int a[20], n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)    //注意，这里数组从a[1]开始存储的
+	{
+		scanf("%d", &a[i]);
+	}
+
+	quicksort(1, n,a);
+
+	for (int i = 1; i <= n; i++)
+		printf("%d", a[i]);
+
+	getchar(); getchar();
+
+}
+
+
