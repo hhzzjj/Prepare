519a5be3d34bc454c2dab32b7139a33ab213cfa4
hhzzjj <37446568+hhzzjj@users.noreply.github.com>
2019-02-15 17:52:33 +0800

    Update OrderTraverseBitree.cpp

--- OrderTraverseBiTree/OrderTraverseBitree.cpp
+++ OrderTraverseBiTree/OrderTraverseBitree.cpp
@@ -1,1 +1,38 @@
-
+/*
+struct TreeNode {
+    char data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};*/
+ 
+ 
+ 
+ 
+class Solution {
+public:
+    void PreOrder(TreeNode *pRoot)
+    {
+        cout<<pRoot->data<<endl;
+        PreOrder(pRoot->left);
+        PreOrder(pRoot->right);
+    }//前序遍历
+  
+  
+   void InOrder(TreeNode *pRoot)
+    {
+        InOrder(pRoot->left);
+        cout<<pRoot->data<<endl;
+        InOrder(pRoot->right);
+    }//中序遍历
+  
+  
+   void PostOrder(TreeNode *pRoot)
+    {
+        PostOrder(pRoot->left);
+        PostOrder(pRoot->right);
+        cout<<pRoot->data<<endl;
+    }//后序遍历
+};
