bade64a51bbbcd29a38546c0cf72f0435d274399
hhzzjj <37446568+hhzzjj@users.noreply.github.com>
2019-02-15 17:42:56 +0800

    Update CreateBiTree.cpp

--- CreateBiTree/CreateBiTree.cpp
+++ CreateBiTree/CreateBiTree.cpp
@@ -1,1 +1,30 @@
-
+/*
+struct TreeNode {
+    char data;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};*/
+ 
+ 
+ 
+ 
+class Solution {
+public:
+    void CreateBiTree(TreeNode *pRoot)
+    {
+        char c;
+        cin>>c;
+        if(c=='#')
+          pRoot=NULL;
+        else
+        {
+          pRoot=new TreeNode;
+          pRoot->data=c;
+          CreateBiTree(pRoot->left);
+          CreateBiTree(pRoot->right);
+        }
+    }
+};
