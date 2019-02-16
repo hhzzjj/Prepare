24c8e6bb09b963004b349792cf15b60f93b3582b
hhzzjj <37446568+hhzzjj@users.noreply.github.com>
2019-02-16 23:10:42 +0800

    Update LevelOrder.cpp

--- LevelOrder(层序遍历)/LevelOrder.cpp
+++ LevelOrder(层序遍历)/LevelOrder.cpp
@@ -19,7 +19,7 @@ public:
         int f=0;
         int r=0;                  //不足2：用了很多变量，很绕。r为存入队列的个数，r为当前队列已经输出个数，p指向第r个，为树节点的地址
         if(root!=NULL)
-            queue[++r]=root;
+            queue[++r]=root;      //不足3：用的伪队列......
         while(f!=r)
         {
             TreeNode *p=queue[++f];
@@ -28,6 +28,30 @@ public:
                 queue[++r]=p->left;
             if(p->right!=NULL)
                 queue[++r]=p->right;
+        }
+        return result;
+    }
+};
+
+
+//看了他人的思路后，修改的（运用队列，每得出一个数据，就push该节点的左右孩子，然后再pop该节点）
+class Solution {
+public:
+    vector<int> PrintFromTopToBottom(TreeNode* root)
+    {
+        vector<int>result;;
+        if(root==NULL)
+            return result;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            result.push_back(q.front()->val);
+            if(q.front()->left!=NULL)
+                q.push(q.front()->left);
+            if(q.front()->right!=NULL)
+                q.push(q.front()->right);
+            q.pop();
         }
         return result;
     }
